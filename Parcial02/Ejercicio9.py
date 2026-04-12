500446e8b94fc6879831b58b1f9708be459dbd90
Cesar-oliver <cesarolivermejia789@gmail.com>
2026-04-11 18:47:34 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 9, reemplazo de texto y conversión a mayúsculas

---
+++ Parcial02/Ejercicio9.py
@@ -0,0 +1,10 @@
+# 1. Cadena original
+texto = "Any time y Anytime"
+
+# 2. Reemplazar "Any time" por "Always"
+texto_nuevo = texto.replace("Any time", "Always")
+
+# 3. Convertir a mayúsculas
+resultado = texto_nuevo.upper()
+
+print(resultado)
