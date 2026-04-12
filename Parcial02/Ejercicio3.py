f761a2d5535fe51a3cfd1cdb98dd023a0757a06d
Cesar-oliver <cesarolivermejia789@gmail.com>
2026-04-11 18:37:26 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 3, remover prefijo y convertir a mayúsculas

---
+++ Parcial02/Ejercicio3.py
@@ -0,0 +1,11 @@
+# Paso 1: guardar el texto
+texto = "ING. Cesar"
+
+# Paso 2: quitar "ING. "
+texto = texto.replace("ING. ", "")
+
+# Paso 3: convertir todo a mayúsculas
+texto = texto.upper()
+
+# Mostrar resultado
+print(texto)
