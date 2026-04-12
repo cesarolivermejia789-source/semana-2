d803c44009459dfb1f77a052a2ec970707745c46
Cesar-oliver <cesarolivermejia789@gmail.com>
2026-04-11 18:46:54 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 8, contar letras y dividir texto en líneas

---
+++ Parcial02/Ejercicio8.py
@@ -0,0 +1,12 @@
+# 1. Bloque de texto de 3 líneas
+texto = """Amor constante más allá de la muerte
+Cerrar podrá mis ojos la postrera sombra
+Mas no de esotra parte en la ribera"""
+
+# 2. Contar cuántas veces aparece la letra "a"
+contador = texto.count("a")
+print("La letra 'a' aparece:", contador)
+
+# 3. Separar el texto por líneas
+lineas = texto.splitlines()
+print(lineas)
