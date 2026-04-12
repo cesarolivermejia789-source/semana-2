f918e2a90f6f2b2f1d55ae616693b7b9b933d64c
Cesar-oliver <cesarolivermejia789@gmail.com>
2026-04-11 18:35:33 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 2, capitalizar texto y reemplazar palabras

---
+++ Parcial02/Ejercicio2.py
@@ -0,0 +1,11 @@
+# Paso 1: guardar el texto
+texto = "Cesar"
+
+# Paso 2: poner mayúscula al inicio de cada palabra
+texto = texto.title()
+
+# Paso 3: cambiar "nombre" por "apellido"
+texto = texto.replace("Cesar", "Salazar")
+
+# Mostrar resultado
+print(texto)
